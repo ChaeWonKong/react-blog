f334971d3a1dfb361d2cfd6a76b5e911cd91617b
Kong Chae Won <chaewonkong@gmail.com>
2020-12-28 12:20:00 +0900

    style: 💄 fix lint

--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import Header from './Header';
-import Helment from 'react-helmet'
+import Helment from 'react-helmet';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -40,13 +40,16 @@ const Wrapper = styled.div`
 const Layout = ({ children }: LayoutProps) => (
   <>
     <Helment>
-        <meta property='og:title' content={ `TIN WOODMAN :: Tin Woodman's bildungsroman blog` } />
-        <meta property='og:description' content={ 'A Blog For tech, IT, Computer Engineering, Web and React.' } />
-        <meta
-          name='keywords'
-          content={ 'IT, computer science, web, react, nodejs, engineering, programming, development' }
-        />
-        <meta property='og:image' content='/tinamn.jpg' />
+      <meta property='og:title' content={ `TIN WOODMAN :: Tin Woodman's bildungsroman blog` } />
+      <meta
+        property='og:description'
+        content={ 'A Blog For tech, IT, Computer Engineering, Web and React.' }
+      />
+      <meta
+        name='keywords'
+        content='IT, computer science, web, react, nodejs, engineering, programming, development'
+      />
+      <meta property='og:image' content='/tinamn.jpg' />
     </Helment>
     <GlobalStyle />
     <Header />
